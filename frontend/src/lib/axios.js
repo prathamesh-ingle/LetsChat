fc451a495514b0e7a3bc3380581a28f4a8d4645f
prathamesh <134064798+prathamesh-ingle@users.noreply.github.com>
2026-01-06 00:38:22 +0530

    Fix axios + CORS

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
-//src/lib/axios.js
-import axios from "axios";
-
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.DEV 
+  ? "http://localhost:5001/api" 
+  : import.meta.env.VITE_API_BASE_URL || '/api';
 
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true, // Send cookies with requests
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
