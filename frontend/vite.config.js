b0ad8f6fe33993b9487e06cfd460d58370017ae7
prathamesh <134064798+prathamesh-ingle@users.noreply.github.com>
2026-01-16 17:16:41 +0530

    updated vite.config.js for video call in production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',  // Stream SDK fix
+  },
+  optimizeDeps: {
+    include: [
+      '@stream-io/video-react-sdk',
+      '@stream-io/video-client',
+      'react', 'react-dom'
+    ]
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          stream: ['@stream-io/video-react-sdk']
+        }
+      }
+    }
+  }
 })
